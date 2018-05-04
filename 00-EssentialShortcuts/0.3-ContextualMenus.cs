b0b4d0a7e120191c4e436bb6c24ce62b8486bd63
yusjoel <joelyusj@hotmail.com>
2018-05-04 16:16:10 +0800

    翻译 00-EssentialShortcuts/0.3-ContextualMenus.cs

--- 00-EssentialShortcuts/0.3-ContextualMenus.cs
+++ 00-EssentialShortcuts/0.3-ContextualMenus.cs
@@ -1,66 +1,63 @@
-﻿namespace EssentialShortcuts
+namespace EssentialShortcuts
 {
-    // Navigate To menu
+    // "导航到"菜单
     //
-    // Displays a contextual menu of options you can use to navigate to from
-    // your current location
-    //
-    // Very useful way of navigating without having to learn ALL of the shortcuts!
+    // 显示一个上下文相关的菜单, 其中列出你可以从当前位置导航去的选项
+    // 如果你记不住快捷键这个功能非常有用!
     //
     // Alt+` (VS)
     // Ctrl+Shift+G (IntelliJ)
     //
-    // 1. Place text caret on class name, invoke menu
-    // 2. Alt+Enter on class name, start typing (e.g. "Navigate To", "Extension methods")
+    // 1. 将光标放在下面的类名上, 按快捷键弹出菜单
+    //    选择Extension Methods
+    // 2. 也可以在类名上按Alt+Enter, 输入"Navigate To", 或者直接输入"Extension Methods"
     //
-    // (Most options not useful with this example. Covered in more detail in next section)
+    // 更多关于导航的内容会在01 Navigation中介绍
     public class NavigateTo
     {
     }
 
 
-    // Refactor This menu
+    // "重构这里"菜单
     //
-    // Displays a contextual menu of options for refactoring available at the current location
-    //
-    // Very useful for invoking refactorings without requiring to know ALL shortcuts
+    // 显示一个上下文相关的菜单, 其中列出你可以从当前位置进行的重构选项
+    // 如果你记不住快捷键这个功能非常有用!
     //
     // Ctrl+Shift+R (VS + IntelliJ)
     //
-    // 3. Place text caret on class name, invoke menu
-    //    Select rename, rename class
-    // 4. Alt+Enter on class name, start typing (e.g. "Refactor This", "Rename")
+    // 3. 将光标放在下面的类名上, 按快捷键弹出菜单
+    //    选择Rename
+    // 4. 也可以在类名上按Alt+Enter, 输入"Refactor This", 或者直接输入"Rename"
     //
-    // Covered in more detail in next section
+    // 更多关于重构的内容会在04 Refactoring中介绍
     public class RefactorThis
     {
     }
 
 
-    // Generate Code menu
+    // "生成代码"菜单
     //
-    // Displays a contextual menu of options for generating code available at the current location
+    // 显示一个上下文相关的菜单, 其中列出你可以从当前位置生成代码的选项
     //
-    // Alt+Insert (in text editor)
+    // Alt+Insert
     //
-    // Covered in more detail in the Editing section
+    // 更多细节会在02 Editing中介绍
     public class GenerateCode
     {
     }
 
-    // Inspect This menu
+    // "审查这里"菜单
     //
-    // Displays a contextual menu of options for analysing code and control flow
-    // at the current location
-    //
-    // Very useful for invoking value analysis without learning ALL shortcuts!
+    // 显示一个上下文相关的菜单, 其中列出你可以从当前位置分析代码、控制流的选项
+    // 如果你记不住快捷键这个功能非常有用!
     //
     // Ctrl+Shift+Alt+A (VS + IntelliJ)
     //
-    // 4. Place text caret on class name, invoke menu
-    // 5. Alt+Enter on class name, start typing (e.g. "Inspect This", "Hierarchies")
+    // 5. 将光标放在下面的类名上, 按快捷键弹出菜单
+    //    选择Hierarchies
+    // 6. 也可以在类名上按Alt+Enter, 输入"Inspect This", 或者直接输入"Hierarchies")
     //
-    // (Most options not useful with this example. Covered in more detail in later section)
+    // 更多关于审查的内容会在03 Inspections中介绍
     public class InspectThis
     {
     }
